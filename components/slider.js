9715eed217722c7a54e58dcd9564bdfce601fb56
Asmovic <asogba007@gmail.com>
2022-07-12 18:15:44 +0100

    Add react-typed to slider component

--- components/slider.js
+++ components/slider.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/display-name */
 import React, { useState, useEffect, useRef } from 'react'
-
+import Typed from 'react-typed';
 const carouselImages = [
     './images/morkaz.jpg',
     './images/mudir.jpg',
@@ -55,7 +55,7 @@ export default function Slider(){
             <div className="aspect-w-16 aspect-h-9">
                 <img src={carouselImages[currentIndex]} alt="image" />
             </div>
-            <div className="absolute w-full top-1/2 transform -translate-y-1/2 px-3 flex justify-between items-center">
+            <div className="absolute w-full top-1/2 transform z-10 -translate-y-1/2 px-3 flex justify-between items-center">
                 <button onClick={handleOnPrevClick} className="bg-black text-white p-1 rounded-full bg-opacity-50 
                     cursor-pointer hover:bg-opacity-100 transition">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
@@ -69,6 +69,28 @@ export default function Slider(){
                     </svg>
                 </button>
             </div>
+            <div className="absolute w-full top-1/2 z-5 transform -translate-y-1/2 px-3 flex justify-center items-center">
+                <h1 className="font-extrabold text-xl md:text-5xl lg:text-6xl">
+                Salam Aleykun! <span className="text-md font-medium">👋</span>, Welcome to{' '}
+                <span className="text-blue-500 shadow-md custom-box-shadow">
+                    Morkaz.
+                </span>
+                <span className="block text-lg pt-4 md:text-4xl lg:text-4xl font-normal text-center">
+                    <Typed
+                    strings={[
+                        "Committed To Excellence In Islamic Education",
+                        "Opening doors through literacy.",
+                    ]}
+                    loop={true}
+                    typeSpeed={50}
+                    backSpeed={20}
+                    backDelay={3000}
+                    smartBackspace={false}
+                    cursorChar={'_'}
+                    />
+                </span>
+            </h1>
+        </div>
         </div>
     )
 }
--- components/toggle.js
+++ components/toggle.js
@@ -4,11 +4,25 @@ if (typeof window === 'object') {
     const html =  document.querySelector("html");
     console.log('html...', html)
     const toggleDarkMode = function(){
-        checkbox.checked ? html.classList.add("dark")
-        : html.classList.remove("dark");
+        checkbox.checked ? (
+            html.classList.add("dark"),
+            localStorage.setItem("theme", "dark")
+        )
+        : (
+            html.classList.remove("dark"),
+            localStorage.setItem("theme", "light")
+        );
+  
     }
     
     toggleDarkMode();
     
     checkbox.addEventListener('click', toggleDarkMode);
+
+/*             // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark')
+              } else {
+                document.documentElement.classList.remove('dark')
+              } */
 }
